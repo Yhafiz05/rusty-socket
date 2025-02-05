8c3138d58e2d4029756aaee1bad301a964ca9e25
Yhafiz05 <hyaolire@gmail.com>
2025-02-05 21:18:43 +0100

    Echo server

---
+++ README.md
@@ -0,0 +1,16 @@
+# A simple tcp socket in Rust
+
+You can find in the main.rs a simple implementation of a tcp server.
+The server can handle multiple request thanks to thread.<br>
+It is an echo server, it just reply to your message with your message.
+## To test this project
+
+- Get the nc command in your terminal
+
+## Commmands
+
+```bash
+    nc localhost 8080
+```
+
+
---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::net::{Shutdown, TcpListener, TcpStream};
+use std::thread;
+use std::net::{Ipv4Addr,SocketAddrV4};
+use std::io::{BufRead, BufReader, BufWriter, Write};
+
+const ADDR: Ipv4Addr = Ipv4Addr::new(127, 0, 0, 1);
+const PORT: u16 = 8080;
+fn main() {
+    let tcpListener = TcpListener::bind(SocketAddrV4::new(ADDR,PORT)).unwrap();
+    println!("Listening on port {}", PORT);
+    for stream in tcpListener.incoming() {
+        // Error handling
+        match stream {
+            Ok(stream) => {
+                // move the ownership of "stream" to the thread
+                thread::spawn(move || {
+                    handle_client(stream)
+                });
+            }
+            Err(e) => {
+                eprint!("Connection failed cause by {}", e);
+            }
+        }
+    }
+
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut readline = BufReader::new(&stream);
+    let mut writeline  = BufWriter::new(&stream);
+    let mut line = String::new();
+    while match readline.read_line(&mut line){
+        Ok(n) => {
+            writeln!(writeline, "{}", line.trim_end()).unwrap();
+            writeline.flush().unwrap();
+            line.clear();
+            true
+        },
+        Err(e) => {
+            eprint!("AN error occurred reading from stream: {}", e);
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    }{}
+    stream.shutdown(Shutdown::Both).unwrap()
+}
